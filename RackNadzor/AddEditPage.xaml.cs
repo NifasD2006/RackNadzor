1b195a97ffda9ad8d34b473a64ed101f84389d3f
Danis <danis.safin.99@list.ru>
2025-10-19 02:11:19 +0500

    AddPage

---
+++ RackNadzor/AddEditPage.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RackNadzor
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+
+        private Devices _currentDevice = new Devices();
+        public AddEditPage()
+        {
+            InitializeComponent();
+            DataContext = _currentDevice;
+        }
+
+
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            DataInspect.SelectedDate = DateTime.Now;
+        }
+
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+
+            if (string.IsNullOrEmpty(_currentDevice.DeviceModel))
+                errors.AppendLine("Укажите название модели");
+
+            if (string.IsNullOrEmpty(_currentDevice.DeviceSerialNumber))
+                errors.AppendLine("Укажите серийный номер");
+
+            if (string.IsNullOrEmpty(_currentDevice.DeviceIP))
+                errors.AppendLine("Укажите IP-Адрес");
+            else if (!System.Net.IPAddress.TryParse(_currentDevice.DeviceIP, out _))
+                errors.AppendLine("Некорректный формат IP-адреса");
+
+            if (string.IsNullOrEmpty(_currentDevice.DevicePositionInUnits.ToString()) && _currentDevice.DevicePositionInUnits>0 && _currentDevice.DevicePositionInUnits > 48)
+                errors.AppendLine("Укажите название позицию в стойке");
+
+            if (TypeCombo.SelectedItem == null)
+                errors.AppendLine("Выберите тип устройства");
+
+            if (ManufacturerCombo.SelectedItem == null)
+                errors.AppendLine("Выберите производителя");
+
+            if (RaydCombo.SelectedItem == null)
+                errors.AppendLine("Выберите ряд");
+
+            if (NomerCombo.SelectedItem == null)
+                errors.AppendLine("Выберите номер");
+
+            if (!DataInspect.SelectedDate.HasValue)
+                errors.AppendLine("Укажите дату технического осмотра");
+
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            _currentDevice.DeviceManufacturer = ManufacturerCombo.Text;
+            _currentDevice.DeviceTypeID = TypeCombo.SelectedIndex + 1;
+            _currentDevice.DeviceRackName = RaydCombo.Text + NomerCombo.Text;
+            _currentDevice.DeviceInspectionDate = DataInspect.SelectedDate;
+            _currentDevice.DeviceSizeInUnits = 1;
+            _currentDevice.DeviceDataCenters = 1;
+
+            if (_currentDevice.DeviceID == 0)
+            {
+                RackBDEntities.GetContext().Devices.Add(_currentDevice);
+            }
+
+            try
+            {
+                RackBDEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+                Manager.MainFrame.GoBack();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+    }
+}
--- RackNadzor/DevicePage.xaml.cs
+++ RackNadzor/DevicePage.xaml.cs
@@ -22,29 +22,91 @@ namespace RackNadzor
     {
         public DevicePage()
         {
+
+
             InitializeComponent();
             var currentDevices = RackBDEntities.GetContext().Devices.ToList();
 
             DeviceListView.ItemsSource = currentDevices;
+            int MaxCountDevices = DeviceListView.Items.Count;
+            int CountDevices = DeviceListView.Items.Count;
+            WendorComboBox.SelectedIndex = 0;
+            MaxCountTBlock.Text = MaxCountDevices.ToString();
+            UpdateDevices();
+        }
+
+
+        private void UpdateDevices()
+        {
+            var currentDevices = RackBDEntities.GetContext().Devices.ToList();
+
+            if (WendorComboBox.SelectedIndex == 1)
+                currentDevices = currentDevices.Where(p => p.DeviceManufacturer== "Dell").ToList();
+            if (WendorComboBox.SelectedIndex == 2)
+                currentDevices = currentDevices.Where(p => p.DeviceManufacturer == "Cisco").ToList();
+            if (WendorComboBox.SelectedIndex == 3)
+                currentDevices = currentDevices.Where(p => p.DeviceManufacturer == "Supermicro").ToList();
+            if (WendorComboBox.SelectedIndex == 4)
+                currentDevices = currentDevices.Where(p => p.DeviceManufacturer == "HP").ToList();
+            if (WendorComboBox.SelectedIndex == 5)
+                currentDevices = currentDevices.Where(p => p.DeviceManufacturer == "Lenovo").ToList();
+
+            //currentDevices=currentDevices.Where(p => p.DeviceManufacturer.ToLower().Contains(TextBoxSearch.Text.ToLower() || p.DeviceDomainName.ToLower().Contains(TextBoxSearch.Text.ToLower())).ToList();
+
+            //currentDevices = currentDevices.Where(p => p.DeviceManufacturer.ToLower().Contains(TextBoxSearch.Text.ToLower()) || p.DeviceRackName.ToLower().Contains(TextBoxSearch.Text.ToLower()) || p.DeviceModel.ToLower().Contains(TextBoxSearch.Text.ToLower()) || p.DeviceTypes.DeviceTypeName.ToLower().Contains(TextBoxSearch.Text.ToLower()) ||
+            //    p.DeviceSerialNumber.ToLower().Contains(TextBoxSearch.Text.ToLower())  || p.DeviceDomainName.ToLower().Contains(TextBoxSearch.Text.ToLower()) || p.DeviceIP.ToLower().Contains(TextBoxSearch.Text.ToLower())).ToList();
+
+            currentDevices = currentDevices.Where(p => p.DeviceModel.ToLower().Contains(TextBoxSearch.Text.ToLower()) || p.DeviceTypes.DeviceTypeName.ToLower().Contains(TextBoxSearch.Text.ToLower()) ||
+             p.DeviceSerialNumber.ToLower().Contains(TextBoxSearch.Text.ToLower()) || (p.DeviceDomainName != null && p.DeviceDomainName.ToLower().Contains(TextBoxSearch.Text.ToLower())) || p.DeviceIP.ToLower().Contains(TextBoxSearch.Text.ToLower())).ToList();
+
+            if (RButtonDown.IsChecked.Value)
+            {
+                currentDevices = currentDevices.OrderByDescending(p => p.DeviceInspectionDate).ToList();
+            }
+
+            if (RButtonUP.IsChecked.Value)
+            {
+                currentDevices = currentDevices.OrderBy(p => p.DeviceInspectionDate).ToList();
+            }
+
+            DeviceListView.ItemsSource = currentDevices;
+            int CountDevices = DeviceListView.Items.Count;
+            CountTBlock.Text = CountDevices.ToString();
         }
 
         private void RButtonUP_Checked(object sender, RoutedEventArgs e)
         {
-
+            UpdateDevices();
         }
 
         private void RButtonDown_Checked(object sender, RoutedEventArgs e)
         {
-
+            UpdateDevices();
         }
 
         private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            UpdateDevices();
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            UpdateDevices();
+        }
+
+        private void RefrashBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TextBoxSearch.Clear();
+            WendorComboBox.SelectedIndex = 0;
+            RButtonUP.IsChecked = false;
+            RButtonDown.IsChecked = false;
+            UpdateDevices();
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            Manager.MainFrame.Navigate(new AddEditPage());
 
         }
     }
